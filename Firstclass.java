d73a089030248d47a963adcb4a387c7a45640f5a
HosseinBP1 <hosseinbabaeipour17@gmail.com>
2024-05-23 20:36:08 +0330

    Create Firstclass.java

---
+++ Firstclass.java
@@ -0,0 +1,12 @@
+// Firstclass.java
+
+public class Firstclass {
+// This is called main class which must be named the same way with the file.
+	public static void main(String[] args) {
+	// Here starts the method "main".
+		System.out.println("Hello world!!");
+		// Just simply printing "Hello world!"
+	}
+	// Here ends method "main".
+}
+// And here ends the main class namely "Firstclass".
