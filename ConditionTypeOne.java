14a49d9ba4cafb021d00b433635aedef492b9449
HosseinBP1 <hosseinbabaeipour17@gmail.com>
2024-05-26 19:05:10 +0330

    Update ConditionTypeOne.java

--- ConditionTypeOne.java
+++ ConditionTypeOne.java
@@ -8,15 +8,15 @@ public class ConditionTypeOne {
 		
 		if (A>B)
 			System.out.println("88 is grater than 89");
-		// If the variable A is grater than B, then the out put is "88 is grater than 89".
+			// If the variable A is grater than B, then the out put is "88 is grater than 89".
 		
 		else if (A<B)
 			System.out.println("89 is greater than 88");
-		// If the vice versa is true, "89 is greater than 88" will be printed.
+			// If the vice versa is true, "89 is greater than 88" will be printed.
 		
 		else
 			System.out.println("88 is equal to 89");
-		/* If none of the variables is grater and there is an equivalence, then there will be
-		"88 is equal to 89" printed. */
+			/* If none of the variables is grater and there is an equivalence, then there will be
+			"88 is equal to 89" printed. */
 	}
 }
